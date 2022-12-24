14686efa54e5a3641816cf007b9692d28c6b009e
minaripenguin <minaripenguin@users.noreply.github.com>
2022-12-24 23:04:33 +0800

    SystemUI: Run mobile signal controller callbacks on main thread
    * updating multiple icons outside main thread can cause ui lags taking in consideration that google annotated album art scaling to main thread (which is expensive), running mobile signal controller on main thread might be better as well since we need to update the icons state at real time
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -82,7 +82,6 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
     private final String mNetworkNameDefault;
     private final String mNetworkNameSeparator;
     private final ContentObserver mObserver;
-    private final Handler mHandler = new Handler();
     // Save entire info for logging, we only use the id.
     final SubscriptionInfo mSubscriptionInfo;
     private Map<String, MobileIconGroup> mNetworkToIconLookup;
@@ -402,7 +401,9 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
 
     @Override
     public void notifyListeners(SignalCallback callback) {
-        mHandler.post(() -> {
+        new Handler(mContext.getMainLooper()).post(new Runnable() {
+        @Override
+        public void run() {
         // If the device is on carrier merged WiFi, we should let WifiSignalController to control
         // the SysUI states.
         if (mNetworkController.isCarrierMergedWifi(mSubscriptionInfo.getSubscriptionId())) {
@@ -440,7 +441,8 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 sbInfo.showTriangle,
                 mCurrentState.isDefault);
         callback.setMobileDataIndicators(mobileDataIndicators);
-        });
+        }
+    });
     }
 
     private QsInfo getQsInfo(String contentDescription, int dataTypeIcon) {
